f6a68c984a826add71cf7317f15c99e06ef8eacc
吴超 <541847041@qq.com>
2021-03-22 13:52:34 +0800

    <设计模式>:观察者模式

--- test/js/design-pattern/01_publish-subscribe/observer.js
+++ test/js/design-pattern/01_publish-subscribe/observer.js
@@ -1,56 +1,43 @@
-/**
- * 创建订阅-发布模式实例
- * create()方法创建出的实例是单例模式，每个命名空间都有一个独立的单例对象。
- */
-const PSManager = (function () {
-    var namespacesCache = {}
-    var listenerMap = new Map();// key为消息类型
+// 主题
+var Subject = (function () {
+    var namespacesCache = {};// 命名空间
+    var observerMap = new Map();// 观察者集合
+    var registObserver = function (type, func) {
+        observerMap.get(type) ? observerMap.get(type).push(func) : observerMap.set(type, [func])
+    }
     /**
-     * 发布消息
-     * @param {string} type   消息类型
-     * @param  {...any} args 消息内容
+     * 取消观察者
+     * @param {string} type 
+     * @param {Function} func 观察者
      */
-    var publish = function(type, ...args){
-        if (listenerMap.get(type)) {
-            var listeners = listenerMap.get(type);
-            listeners.forEach(callback => {// 订阅池里的所有事件全部触发
-                callback && callback.apply(null, args)
-            });
+    var removeObserver = function (type, func) { 
+        if (removeObserver.get(type)) {
+            removeObserver.get(type) = removeObserver.get(type).filter(fn => fn !== func)
+        } 
+    }
+    /**
+     * 通知观察者
+     * @param {string} type 
+     */
+    var notifyObservers = function (type, ...args) {
+        if (observerMap.get(type)) {
+            Array.prototype.forEach.apply(observerMap.get(type), [function(callback,i,ary){
+                callback && callback.apply(null,args);// 执行回调
+            }])
         }
     }
     /**
-     * 订阅消息
-     * @param {string} type     消息类型
-     * @param {*} callback      收到消息后执行回调函数
-     */
-    var subscribe = function(type, callback){
-        if (listenerMap.get(type)) {
-            var listeners = listenerMap.get(type);
-            listeners.push(callback)// 添加新的订阅
-        } else {
-            listenerMap.set(type, [callback])
-        }
-    }
-    /**
-     * 创建一个发布-订阅模式的实例
-     * @param {*} namespace     命名空间，解决多个模块调用出现冲突,不传默认'_default'
-     * @returns 实例
+     * 创建主题
+     * @param {string} namespace 
+     * @returns 
      */
     var create = function(namespace = '_default'){
-        return namespacesCache[namespace] ? namespacesCache[namespace] : namespacesCache[namespace] = { publish, subscribe }
-    }
-    /**
-     * 取消订阅
-     * @param {string} type     消息类型
-     * @param {Function} func   
-     */
-    var unsubscribe = function(type, func){
-        if (listenerMap.get(type)){
-            listenerMap.get(type) = listenerMap.get(type).filter(fn => fn !== func)
-        }
+        return namespacesCache[namespace] ? namespacesCache[namespace] : namespacesCache[namespace] = {registObserver, removeObserver,notifyObservers}
     }
     return { create }
+
 })()
+
 // 状态管理器，存储商品数量、单价，总消费价格等信息
 var State = function(){
     var state = {
@@ -91,27 +78,26 @@ window.onload = function () {
     var lies = document.getElementsByTagName('li')
     // 发布消息
     btns[0].onclick = function () {// 购买手机
-        console.log(PSManager.create('order_list'))
-        PSManager.create('order_list').publish('phone',1)
+        Subject.create('order_list').notifyObservers('phone',1)
     }
     btns[1].onclick = function () {// 购买玩具
-        PSManager.create('order_list').publish('toy',1)
+        Subject.create('order_list').notifyObservers('toy',1)
     }
     btns[2].onclick = function () {// 购买鞋子
-        PSManager.create('order_list').publish('shoes',1)
+        Subject.create('order_list').notifyObservers('shoes',1)
     }
     // 订阅消息
-    PSManager.create('order_list').subscribe('phone',function(count){
+    Subject.create('order_list').registObserver('phone',function(count){
         lies[0].innerText = State.add('phone', count).getTip('phone')[0];// 刷新通知栏
         spans[0].innerText = `总商品数：${State.sum()}`;// 刷新通知栏
         spans[1].innerText = State.getTip('phone')[1]// 刷新订单列表
     })
-    PSManager.create('order_list').subscribe('toy',function(count){
+    Subject.create('order_list').registObserver('toy',function(count){
         lies[1].innerText = State.add('toy', count).getTip('toy')[0];
         spans[0].innerText = `总商品数：${State.sum()}`;
         spans[1].innerText = State.getTip('toy')[1]
     })
-    PSManager.create('order_list').subscribe('shoes',function(count){
+    Subject.create('order_list').registObserver('shoes',function(count){
         lies[2].innerText = State.add('shoes', count).getTip('shoes')[0];
         spans[0].innerText = `总商品数：${State.sum()}`;
         spans[1].innerText = State.getTip('shoes')[1]
